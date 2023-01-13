01019ba6056acf4933d1014c972e627d2a94ec71
Artur Lesnik <arturlesnik@gmail.com>
2023-01-13 17:58:20 +0100

    style changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="min-h-screen h-full hero overflow-x-hidden">
       <header className="text-center font-[500] bg-gradient-to-b from-orange-900 to-transparent bg-opacity-50 py-[1rem] grid place-content-center text-white">
       <h1 className="text-[clamp(2rem,3.5vw,4.5rem)]">Weather App</h1>
-      <p className="font-thin text-lg sm:text-2xl leading-[15px]">Everything about weather, wherever you are</p>
+      <p className="font-thin text-lg sm:text-2xl leading-[25px]">Everything about weather, wherever you are</p>
       </header>
       <main className="max-w-[400px] md:max-w-[850px] mx-auto">
       <SearchForm/>
